8b6825b4d578408a53c3b000726dc08f8caffb39
hirosassa <hiro.sassa@gmail.com>
2022-11-25 07:33:43 +0900

    add variable type and permission

--- modules/team/variables.tf
+++ modules/team/variables.tf
@@ -1,5 +1,24 @@
-variable "name" {}
-variable "description" {}
-variable "privacy" {}
-variable "members" {}
-variable "permissions" {}
+variable "name" {
+  type        = string
+  description = "The name of the team."
+}
+
+variable "description" {
+  type        = optional(string)
+  description = "A description of the team."
+}
+
+variable "privacy" {
+  type        = optional(string)
+  description = "The level of privacy for the team. Must be one of secret or closed."
+}
+
+variable "members" {
+  type        = set(string)
+  description = "A list of member names."
+}
+
+variable "permissions" {
+  type        = set(string)
+  description = "A list of the permissions of team members regarding the repository. Must be one of `pull`, `triage`, `push`, `maintain` or `admin`."
+}
