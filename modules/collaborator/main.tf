b248a31395c7995078079fcc710f640ecccef4c8
Taiki Hoshino <88569749+mochigenmai@users.noreply.github.com>
2023-11-10 16:56:19 +0900

    Update main.tf

--- modules/collaborator/main.tf
+++ modules/collaborator/main.tf
@@ -8,17 +8,17 @@ terraform {
 }
 
 locals {
-	collaborator_members = [
+  collaborator_members = [
     for v in setproduct(var.members, keys(var.permissions), values(var.permissions)) : {
       username   = v[0]
       repository = v[1]
-			permission = v[2]
+      permission = v[2]
     }
   ]
 }
 
 resource "github_repository_collaborator" "this" {
-	for_each = { for item in local.collaborator_members : "${item.username}_${item.repository}" => item }
+  for_each = { for item in local.collaborator_members : "${item.username}_${item.repository}" => item }
 
   username   = each.value.username
   repository = each.value.repository
